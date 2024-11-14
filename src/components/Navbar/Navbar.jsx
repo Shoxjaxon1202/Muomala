b82cb9eb0783824468cad864e31fa60b25cab866
Shoxjaxon1202 <shoxjaxonabdusalimov1202@gmail.com>
2024-11-14 20:27:54 +0500

    navbar, intro, services done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+
+import "./navbar.scss";
+import logo from "../../assets/img/navbarLogo.png";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleSelectChange = (event) => {
+    if (event.target.value) {
+      navigate(event.target.value);
+    }
+  };
+  return (
+    <div className="navbar">
+      <div className="navbar_wrapper">
+        <div className="navbar_left">
+          <img src={logo} alt="Logo" className="navbar_img" />
+        </div>
+        <ul className="navbar_middle">
+          <li className="navbar_item">
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li className="navbar_item">
+            <select className="navbar_programs" onChange={handleSelectChange}>
+              <option value="">Programs</option>
+              <option value="/programs">
+                International educational programs
+              </option>
+              <option value="/programs">Specialized courses</option>
+              <option value="/programs">Islamic Finance Literacy Course</option>
+              <option value="/programs">Certification program</option>
+            </select>
+          </li>
+          <li className="navbar_item">
+            <NavLink to={"/tools"}>Finance Tools</NavLink>
+          </li>
+          <li className="navbar_item">
+            <NavLink to={"/contact"}>Contact</NavLink>
+          </li>
+        </ul>
+        <div className="navbar_right">
+          <select>
+            <option value="en">Eng</option>
+            <option value="ru">Rus</option>
+            <option value="uz">Uzb</option>
+          </select>
+          <div className="navbar_palka"></div>
+          <button className="navbar_btn">Sign in</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
