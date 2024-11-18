9e9f2d501770fbe77a1eafb2d2e72b5fdb8f50a8
Shoxjaxon1202 <shoxjaxonabdusalimov1202@gmail.com>
2024-11-18 18:54:49 +0500

    login va register full done

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import Select from "react-select";
+import CountryFlag from "react-country-flag";
+import countryList from "react-select-country-list";
 
 import "../Styles/login.scss";
-
 import logo from "../assets/img/navbarLogo.png";
 import brat from "../assets/img/Image.png";
 import qiz from "../assets/img/illustration image.png";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
+  const { register, handleSubmit } = useForm();
+  const countries = countryList().getData();
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const options = countries.map((country) => ({
+    value: country.value,
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <CountryFlag
+          countryCode={country.value}
+          svg
+          style={{ width: "20px", marginRight: "8px" }}
+        />
+        {country.label}
+      </div>
+    ),
+  }));
+
+  const onSubmit = (data) => {
+    console.log("Selected country:", selectedCountry);
+  };
+
+  const handleCountryChange = (selectedOption) => {
+    setSelectedCountry(selectedOption);
+  };
+
   return (
     <div className="login">
       <div className="login_wrapper">
         <div className="login_left">
           <div className="login_left_top">
-            <img src={logo} alt="" className="login_left_img" />
+            <img src={logo} alt="Logo" className="login_left_img" />
             <div className="login_left_box">
               <p className="login_left_text">Questions?</p>
               <h4 className="login_left_title">Ask Diyor</h4>
@@ -26,15 +55,15 @@ const Register = () => {
               Sign in
             </NavLink>
           </div>
-          <form className="login_form">
+          <form className="login_form" onSubmit={handleSubmit(onSubmit)}>
             <span className="login_span">
               <input
                 required
                 placeholder="Enter your name"
                 type="text"
                 className="login_input"
+                {...register("name", { required: true })}
               />
-              <img src="" alt="" className="login_email" />
             </span>
             <span className="login_span">
               <input
@@ -42,8 +71,8 @@ const Register = () => {
                 placeholder="Enter your email"
                 type="email"
                 className="login_input"
+                {...register("email", { required: true })}
               />
-              <img src="" alt="" className="login_email" />
             </span>
             <span className="login_span">
               <input
@@ -51,14 +80,25 @@ const Register = () => {
                 placeholder="Password"
                 type="password"
                 className="login_input"
+                {...register("password", { required: true })}
               />
             </span>
-            <button className="login_btn">Log in</button>
+            <span className="login_span">
+              <Select
+                options={options}
+                placeholder="Select your country"
+                onChange={handleCountryChange}
+                className="select_country"
+              />
+            </span>
+            <button type="submit" className="login_btn">
+              Register
+            </button>
           </form>
         </div>
-        <img src={brat} alt="" className="login_img" />
+        <img src={brat} alt="Illustration" className="login_img" />
         <div className="login_right">
-          <img src={qiz} alt="" className="login_right_img" />
+          <img src={qiz} alt="Illustration" className="login_right_img" />
           <h3 className="login_right_title">
             Welcome to Al Muamalat - Empowering Your Journey in Islamic Finance
           </h3>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 import "./navbar.scss";
 import logo from "../../assets/img/navbarLogo.png";
 
+import en from "../../assets/img/united-kingdom.png";
+import ru from "../../assets/img/russia.png";
+import uz from "../../assets/img/uzbekistan.png";
+
 const Navbar = () => {
   const navigate = useNavigate();
+  const [languageImage, setLanguageImage] = useState(en);
 
   const handleSelectChange = (event) => {
     if (event.target.value) {
       navigate(event.target.value);
+    }
+  };
+
+  const handleSelectLanguageChange = (event) => {
+    const selectedLanguage = event.target.value;
+    if (selectedLanguage === "en") {
+      setLanguageImage(en);
+    } else if (selectedLanguage === "ru") {
+      setLanguageImage(ru);
+    } else if (selectedLanguage === "uz") {
+      setLanguageImage(uz);
     }
   };
 
@@ -23,24 +39,35 @@ const Navbar = () => {
           <li className="navbar_item">
             <NavLink to={"/"}>Home</NavLink>
           </li>
-          <li className="navbar_item">
-            <select className="navbar_programs" onChange={handleSelectChange}>
-              <option className="navbar_item" value="">
-                Programs
-              </option>
-              <option className="navbar_item" value="/programs">
-                International educational programs
-              </option>
-              <option className="navbar_item" value="/programs">
-                Specialized courses
-              </option>
-              <option className="navbar_item" value="/programs">
-                Islamic Finance Literacy Course
-              </option>
-              <option className="navbar_item" value="/programs">
-                Certification program
-              </option>
-            </select>
+          <li className="navbar__li">
+            <p className="navbar__link navbar__droplink">
+              Programs
+              <svg
+                width="12"
+                height="7"
+                viewBox="0 0 12 7"
+                xmlns="http://www.w3.org/2000/svg"
+                className="navbar_arrow">
+                <path d="M5.57939 6.97499C5.44606 6.97499 5.32106 6.95415 5.20439 6.91249C5.08773 6.87082 4.97939 6.79999 4.87939 6.69999L0.279395 2.09999C0.0960612 1.91665 0.00439453 1.68332 0.00439453 1.39999C0.00439453 1.11665 0.0960612 0.883321 0.279395 0.699988C0.462728 0.516654 0.696061 0.424988 0.979395 0.424988C1.26273 0.424988 1.49606 0.516654 1.67939 0.699988L5.57939 4.59999L9.47939 0.699988C9.66273 0.516654 9.89606 0.424988 10.1794 0.424988C10.4627 0.424988 10.6961 0.516654 10.8794 0.699988C11.0627 0.883321 11.1544 1.11665 11.1544 1.39999C11.1544 1.68332 11.0627 1.91665 10.8794 2.09999L6.27939 6.69999C6.17939 6.79999 6.07106 6.87082 5.95439 6.91249C5.83773 6.95415 5.71273 6.97499 5.57939 6.97499Z" />
+              </svg>
+              <div className="navbar__dropdown">
+                <NavLink className={"navbar__link"} to={"/programs"}>
+                  International educational programs
+                </NavLink>
+                <hr className="navbar__hr" />
+                <NavLink className={"navbar__link"} to={"/programs"}>
+                  Specialized courses
+                </NavLink>
+                <hr className="navbar__hr" />
+                <NavLink className={"navbar__link"} to={"/programs"}>
+                  Islamic Finance Literacy Course
+                </NavLink>
+                <hr className="navbar__hr" />
+                <NavLink className={"navbar__link"} to={"/programs"}>
+                  Certification program
+                </NavLink>
+              </div>
+            </p>
           </li>
           <li className="navbar_item">
             <NavLink to={"/tools"}>Finance Tools</NavLink>
@@ -50,7 +77,8 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="navbar_right">
-          <select>
+          <img className="navbar_language" src={languageImage} alt="Language" />
+          <select onChange={handleSelectLanguageChange}>
             <option value="en">Eng</option>
             <option value="ru">Rus</option>
             <option value="uz">Uzb</option>
